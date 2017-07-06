1a5c796d3cd795051ae2dbd613d3480ae5a2f7e5
m-kouda <makotokoda82@gmail.com>
2017-07-06 22:48:11 +0900

    クイズの表示の実装

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
+var button = document.getElementById('button');
+var quizes = document.getElementById('quizes');
 var questions = [
   {
     question: "What is 10/2?",
     answers: {
       a: '3',
       b: '5',
-      c '115'
+      c: '115'
     },
     correctAnswer: 'b'
   },
-]
+  {
+    question: "What is 10/2?",
+    answers: {
+      a: '3',
+      b: '5',
+      c: '115'
+    },
+    correctAnswer: 'b'
+  }
+];
+
+function showquiz() {
+  var outputs = "";
+  for (var i = 0; i < questions.length; i++) {
+    var output = "<li><p>" + questions[i].question + "</p>";
+    for (letter in questions[i].answers) {
+        output += '<input type="radio" name="quiz_' + i + '" value="' + letter + '">' + letter + ": " + questions[i].answers[letter];
+    }
+    output += "</li>";
+    outputs += output;
+  }
+  quizes.innerHTML = outputs;
+}
+
+showquiz();
+
+button.addEventListener('click', function() {
+  console.log(document.getElementsByName('quiz_1'));
+});
